556866694faf45e6e4df1eade14c26597c15d091
Andrey <Dr_Wo1f@mail.ru>
2023-01-31 01:53:33 +0300

    task 13 finished

---
+++ task13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. (Цифры считать справа налево).
+// 645 -> 6
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+
+Console.WriteLine("Введите любое число");
+int count = int.Parse(Console.ReadLine()!);
+if (count > 99)
+{
+    int result = count % 1000;
+    result = result / 100;
+    Console.WriteLine($"Третья цифра {result}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
